66553f579fd8eef6631fc2644ac1f257979f47ea
dclba <dcl.bosna@gmail.com>
2023-09-26 02:27:09 +0200

    Odradjeno zatvaranje projekata.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+     document.querySelector('a[href="#contact"]').addEventListener('click', function (e) {
+            e.preventDefault();
+            const contactSection = document.getElementById('contact');
+            contactSection.scrollIntoView({ behavior: 'smooth' });
+        });
+
+
+
+
+    $(document).ready(function() {
+        // Inicijalno sakrijte sve projektne detalje
+        $(".project-details").hide();
+
+        // Dodajte funkcionalnost za prikazivanje i sakrivanje detalja projekta
+        $(".show-details").on("click", function() {
+            var projectDetails = $(this).siblings(".project-details");
+            $(".project-details").not(projectDetails).slideUp(); // Zatvorite druge otvorene detalje
+            projectDetails.slideToggle();
+        });
+    });
+
     // Dodavanje event listenera za scroll do sekcije "Contact"
-    document.querySelector('a[href="#contact"]').addEventListener('click', function (e) {
+   /* document.querySelector('a[href="#contact"]').addEventListener('click', function (e) {
         e.preventDefault();
         const contactSection = document.getElementById('contact');
         if (contactSection) {
@@ -9,13 +30,66 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Inicijalno sakrijte sve projektne detalje
-    $(".project-details").hide();
+    const projectDetails = document.querySelectorAll('.project-details');
+    projectDetails.forEach(details => {
+        details.style.display = 'none';
+    });
 
     // Dodajte funkcionalnost za prikazivanje i sakrivanje detalja projekta
-    $(".show-details").on("click", function() {
-        var projectDetails = $(this).siblings(".project-details");
-        $(".project-details").not(projectDetails).slideUp(); // Zatvorite druge otvorene detalje
-        projectDetails.slideToggle();
+    const showDetailsButtons = document.querySelectorAll('.show-details');
+    showDetailsButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const projectDetails = this.parentElement.querySelector('.project-details');
+            if (projectDetails) {
+                // Zatvorite sve detalje projekta prije nego što otvorite ovaj
+                projectDetails.parentElement.querySelectorAll('.project-details').forEach(details => {
+                    details.style.display = 'none';
+                });
+
+                // Otvorite ili zatvorite trenutni detalj projekta
+                if (projectDetails.style.display === 'block') {
+                    projectDetails.style.display = 'none';
+                } else {
+                    projectDetails.style.display = 'block';
+                }
+            }
+        });
+    });
+*/
+
+   // Inicijalizujte Lightbox
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+});
+
+
+
+
+/*contactSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+
+    // Inicijalno sakrijte sve projektne detalje
+    const projectDetails = document.querySelectorAll('.project-details');
+    projectDetails.forEach(details => {
+        details.style.display = 'none';
+    });
+
+    // Dodajte funkcionalnost za prikazivanje i sakrivanje detalja projekta
+    const showDetailsButtons = document.querySelectorAll('.show-details');
+    showDetailsButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const projectDetails = this.nextElementSibling;
+            if (projectDetails) {
+                if (projectDetails.style.display === 'block') {
+                    projectDetails.style.display = 'none';
+                } else {
+                    projectDetails.style.display = 'block';
+                }
+            }
+        });
     });
 
     /document.addEventListener('DOMContentLoaded', function() {
@@ -51,3 +125,4 @@ document.addEventListener('DOMContentLoaded', function() {
          }
      });
 
+*/
